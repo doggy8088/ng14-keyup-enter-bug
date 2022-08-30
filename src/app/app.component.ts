29e3018b05dc76997d7b6d4dffe2242f9e93d4b2
Will <doggy.huang@gmail.com>
2022-08-30 17:13:02 +0800

    Demo for keyup.enter bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `<h1>Hello {{ name }}</h1>
+<h2>keyup.enter</h2>
+<p>
+  <input type="text" (keyup.enter)="run($event)">
+</p>
+
+<h2>keyup</h2>
+<p>
+  <input type="text" (keyup)="run($event)">
+</p>
+
+`
 })
-export class AppComponent {
-  title = 'demo1';
+export class AppComponent  {
+  name = 'Angular ' + VERSION.major;
+
+  run($event: any) {
+    alert('You hit: ' + $event.keyCode);
+  }
 }
